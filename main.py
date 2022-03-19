135656977f7c0555ec7da3537447ca6319e7cd70
Mifonsecaa <92172868+Mifonsecaa@users.noreply.github.com>
2022-03-18 21:56:29 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self, color):
+        if (color == "rojo"):
+            self.color = color
+        elif(color == "verde"):
+            self.color = color
+        elif(color == "amarillo"):
+            self.color = color
+        elif(color == "negro"):
+            self.color = color
+        elif(color == "blanco"):
+            self.color = color
+    
+class Auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        
+    def cantidadAsientos(self):
+        for i in self.asientos:
+            if i == "Asiento":
+                Auto.cantidadCreados += 1
+                return Auto.cantidadCreados 
+            else:
+                return Auto.cantidadCreados
+            
+
+
+    def cantidadIntegridad(self):
+        for i in self.asientos:
+            if i.registro == self.registro and self.registro == Motor.cambiarRegistro:
+                return "Auto original"
+            else:
+                return "Las piezas no son originales"
+
+
+class Motor:
+    def __init__(self, numerosCilindros, tipo, registro):
+        self.numerosCilindros = numerosCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+
+    def asignarTipo(self, tipo):
+        if( tipo == "electrico"):
+            self.tipo = tipo
+        elif( tipo == "gasolina"):
+            self.tipo = tipo
